29789867082bd88501d2185ea2321811f948feea
Bue-von-hon <dkssudvn2@gmail.com>
2022-08-23 01:01:33 +0900

    ch11 실습

---
+++ KTH/ch11/BigDecimalTest.java
@@ -0,0 +1,28 @@
+import java.math.BigDecimal;
+
+public class BigDecimalTest {
+    public static void main(String[] args) {
+        /*
+        * 1. BigDecimal의 용도
+        * 불변객체로 정확한 singed 10진수를 표현하는 객체
+        *
+        * 2. 생성자 개수
+        * 약 17개
+        *
+        * 3. abs() 메소드란?
+        * 전달받은 값의 절대값을 반환합니다
+        *
+        * 4. 현재 값에서 특정 값을 빼는 연산을 수행하는 메소드는?
+        *  subtract
+        *
+        * 5. 나누기와 관련된 메소드는 어떤 것들이 있나요?
+        * divide 7가지
+        * remainder
+        * divideToIntegralValue ..?
+        * divideAndRemainder
+        * 등등 여러가지 연산을 제공하고 있다
+        *
+        * */
+        BigDecimal a = new BigDecimal(10);
+    }
+}
