c3d1f0feb14721948be3694e89f2f9794ac0ad3b
Bue-von-hon <dkssudvn2@gmail.com>
2022-09-05 13:52:48 +0900

    챕터13 실습 내용

---
+++ KTH/ch13/list/AbstractList.java
@@ -0,0 +1,3 @@
+public abstract class AbstractList implements list{
+    public abstract void clear();
+}
---
+++ KTH/ch13/list/list.java
@@ -0,0 +1,5 @@
+public interface list {
+    public void add();
+    public void update(int index, Object value);
+    public void remove(int index);
+}
